bb4ca00110a3c91c15d7a0accad2d7f795aa8a95
tct123 <42028373+tct123@users.noreply.github.com>
2022-05-24 19:25:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import requests
 
 API_Key = f3cd11432dabdb4ccae3c0a1347c07a1
 
-lat = 9874293
-lon = 9874293
+print("Breitengrad:")
+lat = input()
+print("Längengrad:")
+lon = input()
+
+request_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
+
+response = requests.get(request_url)
+
