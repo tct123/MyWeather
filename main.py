31c592ba197b34a95205f88b61b5a69cc64246b9
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-21 23:38:02 +0100

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 API_Key = "f3cd11432dabdb4ccae3c0a1347c07a1"
 
@@ -11,16 +12,14 @@ request_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={l
 
 response = requests.get(request_url)
 
-if response.status_code == 200:
-    data =response.json()
-    print(data)
-    weather_describtion = data["weather"][0]["description"]
-    print(weather_describtion)
-    setgraddegree = "Grad"
-    if setgraddegree == "Grad":
-        temperature = round(data["main"]["temp"]-273.15)
-    if setgraddegree == "Fahrenheit":
-        temperature = round(data["main"]["temp"])
-    print(temperature)
-else:
-    print("Fehler")
+
+data =response.json()
+#print(data)
+weather_describtion = data["weather"][0]["description"]
+print(weather_describtion)
+setgraddegree = "Grad"
+if setgraddegree == "Grad":
+    temperature = round(data["main"]["temp"]-273.15)
+if setgraddegree == "Fahrenheit":
+    temperature = round(data["main"]["temp"])
+print(temperature)
