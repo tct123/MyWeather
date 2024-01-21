56ae208c935be41ae88a876b3be8ae85a040f2ee
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-21 23:59:26 +0100

    .

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import requests
 import json
 
-API_Key = "f3cd11432dabdb4ccae3c0a1347c07a1"
-
-print("Breitengrad:")
-lat = input()
-print("Längengrad:")
-lon = input()
-
-request_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
-
-response = requests.get(request_url)
+# API_Key = "f3cd11432dabdb4ccae3c0a1347c07a1"
+#
+# print("Breitengrad:")
+# lat = input()
+# print("Längengrad:")
+# lon = input()
 
 
-data =response.json()
-#print(data)
-weather_describtion = data["weather"][0]["description"]
-print(weather_describtion)
-setgraddegree = "Grad"
-if setgraddegree == "Grad":
-    temperature = round(data["main"]["temp"]-273.15)
-if setgraddegree == "Fahrenheit":
-    temperature = round(data["main"]["temp"])
-print(temperature)
+class MyWeather:
+    def __init__(self, api_key, lat, lon, setgraddegree):
+        self.api_key = api_key
+        self.lat = lat
+        self.lon = lon
+        self.setgraddegree = setgraddegree
+        self.request_url = f"https://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.api_key}"
+        self.response = self.requests.get(self.request_url)
+        self.data = self.response.json()
+        # print(data)
+
+    def description(self):
+        weather_describtion = self.data["weather"][0]["description"]
+        return weather_describtion
+
+    def degreefunc(self):
+        if self.setgraddegree == "Grad":
+            temperature = round(self.data["main"]["temp"] - 273.15)
+        if self.setgraddegree == "Fahrenheit":
+            temperature = round(self.data["main"]["temp"])
+        return temperature
